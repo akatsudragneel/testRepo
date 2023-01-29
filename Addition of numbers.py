75e9a8a4ce2213679dc9c8746f85c7af5deccc15
akatsuDragneel <akashpswamy@gmail.com>
2023-01-29 16:06:56 +0530

    Added comment

--- Addition of numbers.py
+++ Addition of numbers.py
@@ -1,4 +1,4 @@
 a = float(input("Enter a number"))
 b = float(input("Enter another number"))
 
-print ("The sum of {} and {} is {}".format(a, b, a+b))
+print ("The sum of {} and {} is {}".format(a, b, a+b)) #changes done here by adding comment
